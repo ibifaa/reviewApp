e20da572a6625d84d24cad2b61a716513b806456
ibifaa <ibifaaibisakiaruoture@gmail.com>
2023-05-28 07:51:04 +0100

    Created a review app created the index file and added   js file created an array of object that contains name, position and review.

---
+++ script.js
@@ -0,0 +1,31 @@
+let reviewList = [{id: 1, name: "Ibifaa Ibisaki",
+                    jobDescription: "Software Development Couch",       
+                    img:"https://media.licdn.com/dms/image/C4D03AQFkZJcMSDLUvw/profile-displayphoto-shrink_800_800/0/1651667313224?e=2147483647&v=beta&t=LRPjv_Batg26j-jkE4EAHedMsFQAGWsVAFFhKSePXnc",
+                    review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
+                }
+            ,
+                {
+                    id: 2, 
+                    name: "Ibifaa Ibisaki",
+                    jobDescription: "Software Developer",       
+                    img:"https://media.licdn.com/dms/image/C4D03AQFkZJcMSDLUvw/profile-displayphoto-shrink_800_800/0/1651667313224?e=2147483647&v=beta&t=LRPjv_Batg26j-jkE4EAHedMsFQAGWsVAFFhKSePXnc",
+                    review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
+                },
+
+                {
+                id: 3, name: "Kingsly Obot",
+                    jobDescription: "Software Enginner",       
+                    img:"https://media.licdn.com/dms/image/C4D03AQEkn9CR5RMFYQ/profile-displayphoto-shrink_800_800/0/1556881633873?e=1691020800&v=beta&t=zuzjScSNhAWD5tjmDTPqNa5MYOTnZq3PKytJTrLNlso",
+                    review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
+                },
+
+                {
+                    id: 4, name: "Frankly Ogbona",
+                    jobDescription: "Software Enginner",       
+                    img:"https://pbs.twimg.com/profile_images/1276931128457408512/4E1AmVf5_400x400.jpg",
+                    review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
+                }
+                
+        ]
+
+        let image = document.querySelector()
