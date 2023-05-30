1d6c53a65107eabe2d3f659823ca1bd807be1e36
ibifaa <ibifaaibisakiaruoture@gmail.com>
2023-05-30 03:48:08 +0100

    Added an Array of Reviews
    an array of review was created and a function
    that set the image, name and position, an indexNumber
    was also initialized to keep track of the object in the
    array.
    The function parameterized, when the function is called
    and  indexNumber is passed to it as parameter it display details
    of the object.
    the function was called on all the buttons by using event listener
    to listen to the click event on the botton.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let reviewList = [{id: 1, name: "Ibifaa Ibisaki",
-                    jobDescription: "Software Development Couch",       
-                    img:"https://media.licdn.com/dms/image/C4D03AQFkZJcMSDLUvw/profile-displayphoto-shrink_800_800/0/1651667313224?e=2147483647&v=beta&t=LRPjv_Batg26j-jkE4EAHedMsFQAGWsVAFFhKSePXnc",
+let reviewList = [{id: 1, name: "Cristian Florea",
+                    jobDescription: "Software Development Coach",       
+                    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfhjwicKlzU6rj9dQNJf157juZwUSfzdT6Rpf-Tn6RmCZDLv_i_z0boqNcg2GYpPoKIiw&usqp=CAU",
                     review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
                 }
             ,
@@ -8,24 +8,74 @@ let reviewList = [{id: 1, name: "Ibifaa Ibisaki",
                     id: 2, 
                     name: "Ibifaa Ibisaki",
                     jobDescription: "Software Developer",       
-                    img:"https://media.licdn.com/dms/image/C4D03AQFkZJcMSDLUvw/profile-displayphoto-shrink_800_800/0/1651667313224?e=2147483647&v=beta&t=LRPjv_Batg26j-jkE4EAHedMsFQAGWsVAFFhKSePXnc",
+                    image:"./pix.jpg",
                     review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
                 },
 
                 {
                 id: 3, name: "Kingsly Obot",
                     jobDescription: "Software Enginner",       
-                    img:"https://media.licdn.com/dms/image/C4D03AQEkn9CR5RMFYQ/profile-displayphoto-shrink_800_800/0/1556881633873?e=1691020800&v=beta&t=zuzjScSNhAWD5tjmDTPqNa5MYOTnZq3PKytJTrLNlso",
+                    image:"https://media.licdn.com/dms/image/C4D03AQEkn9CR5RMFYQ/profile-displayphoto-shrink_800_800/0/1556881633873?e=1691020800&v=beta&t=zuzjScSNhAWD5tjmDTPqNa5MYOTnZq3PKytJTrLNlso",
                     review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
                 },
 
                 {
                     id: 4, name: "Frankly Ogbona",
                     jobDescription: "Software Enginner",       
-                    img:"https://pbs.twimg.com/profile_images/1276931128457408512/4E1AmVf5_400x400.jpg",
+                    image:"https://pbs.twimg.com/profile_images/1276931128457408512/4E1AmVf5_400x400.jpg",
                     review:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis molestias atque sequi commodi beatae voluptatibus illo optio facere at."
                 }
                 
         ]
 
-        let image = document.querySelector()
+        const image = document.querySelector(".img");
+        const position = document.querySelector(".position");
+        const review = document.querySelector(".comment");
+        const name = document.querySelector(".name")
+
+        const forward = document.querySelector(".forward");
+        const backward = document.querySelector(".backward");
+        const  guess = document.querySelector(".guess")
+
+        // initialization
+        let indexNumber = 0;
+
+       
+        // load initial item
+        window.addEventListener("DOMContentLoaded", ()=>{
+           showDetails(indexNumber);
+           
+        })
+
+        forward.addEventListener("click", ()=>{
+            indexNumber++;
+            if(indexNumber>reviewList.length-1){
+                indexNumber=0
+            }    
+            showDetails(indexNumber);
+        })
+
+        backward.addEventListener("click", () =>{
+            indexNumber--
+            if(indexNumber < 0){
+                indexNumber=reviewList.length-1;
+            } 
+            showDetails(indexNumber);
+        } )
+
+        function showDetails(indexOfPerson){
+            const item = reviewList[indexOfPerson];
+            image.src = item.image;    
+            position.textContent = item.jobDescription;
+            name.textContent = item.name;
+            review.textContent = item.review;
+
+        }
+
+        guess.addEventListener("click", ()=>{
+            let guessReview = Math.floor(Math.random()*reviewList.length)
+           showDetails(guessReview)
+        })
+
+
+        
